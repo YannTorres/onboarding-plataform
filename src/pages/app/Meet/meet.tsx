a17c32617a673b63ec523c412379b59f05ccb2b8
YANN AZEVEDO TORRES <06003743@acad.unifeso.lan>
2024-05-02 20:56:46 -0300

    Inicio do Caledário

--- src/pages/app/Meet/meet.tsx
+++ src/pages/app/Meet/meet.tsx
@@ -1,3 +1,27 @@
+import 'react-day-picker/dist/style.css'
+
+import { useState } from 'react'
+
+import { Calendar } from '@/components/ui/calendar'
+
 export function Meet() {
-  return <h1>Reuniões</h1>
+  const [date, setDate] = useState<Date | undefined>(new Date())
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">Reuniões</h1>
+      <p className="mt-1 text-xl font-semibold">
+        Utilize o nosso calendário para organizar suas reuniões!
+      </p>
+      <div className="mt-10 grid grid-cols-2 items-center justify-center">
+        <Calendar
+          numberOfMonths={1}
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="cell-size-[500px] rounded-md border"
+        />
+        <div></div>
+      </div>
+    </div>
+  )
 }

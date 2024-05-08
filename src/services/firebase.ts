7d355e3bcb7cf3ce426f7f1b9fa48c8fba75f669
Yann <yanngames68@gmail.com>
2024-05-07 23:52:26 -0300

    2 FirebaseAuth

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCPAbz1sUQRccB8Is5EsT7OttpNFieEfz4',
-  authDomain: 'autenticacao-onboarding.firebaseapp.com',
-  projectId: 'autenticacao-onboarding',
-  storageBucket: 'autenticacao-onboarding.appspot.com',
-  messagingSenderId: '396002602997',
-  appId: '1:396002602997:web:92ee1cb4f41bcca9853abc',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
